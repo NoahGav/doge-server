58c97241896d460f0adfa9b5cc42317a01dbaca3
NoahGav <noahgavdev@gmail.com>
2025-02-27 16:53:23 -0600

    Fixed wrong base url in production.

--- site/src/lib/transactions.ts
+++ site/src/lib/transactions.ts
@@ -1,7 +1,6 @@
 import { dev } from "$app/environment";
 
-const base_url = dev ? "http://localhost:3000" : window.location.href;
-
+const base_url = dev ? "http://localhost:3000" : "";
 
 export async function get_transactions() {
     const response = await fetch(`${base_url}/api/transactions`);
